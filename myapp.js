01ca73288a7826b796bdb2c3f367fb48bd70908a
Kirill Kirillov <gorojanen@gmail.com>
2015-03-12 08:37:00 +0000

    12

--- myapp.js
+++ myapp.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var http = require('http');
-var port = process.env.IP;
+var port = process.env.port || 3000;
 var app = express();
 var server = http.createServer(app);
-
-var io = require('socket.io').listen(server);
-
 
 server.listen(port, function() {
    console.log('Listening' + port);
 });
 
+
+var io = require('socket.io').listen(server.io, function () {
+   io.set("transportsxhr-polling");
+   io.set("pollingtion", 10);
+});
+
 app.get('/', function (request, response) {
    response.sendfile(__dirname + '/index.html');
 });
+
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
