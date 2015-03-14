d72afbdfc60f0b86baf9e0e8a6fa70e9fc5a6ba0
Kirill Kirillov <gorojanen@gmail.com>
2015-03-14 07:47:23 +0000

    12:46

--- myapp.js
+++ myapp.js
@@ -9,9 +9,19 @@ server.listen(port, function() {
 });
 
 
+var io = require('socket.io').listen(server.io, function () {
+   io.set("transportsxhr-polling");
+   io.set("pollingtion", 10);
+});
 
 app.get('/', function (request, response) {
    response.sendfile(__dirname + '/index.html');
 });
 
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
